f6c266201c4fda1d96335d4c16876e5cebbb9fcd
Jcohy <jia_chao23@126.com>
2023-02-23 19:08:32 +0800

    fix document

---
+++ java-template/src/main/java/com/jcohy/sample/java/Test.java
@@ -0,0 +1,25 @@
+package com.jcohy.sample.java;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Properties;
+
+/**
+ * 描述: .
+ *
+ * <p>
+ * Copyright © 2023 <a href="https://www.jcohy.com" target= "_blank">https://www.jcohy.com</a>
+ *
+ * @author jiac
+ * @version 1.0.0 2023/2/23 15:45
+ * @since 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        short s1 = 1;
+        s1 = (short) (s1 + 1);
+        s1 += 1;
+    }
+}

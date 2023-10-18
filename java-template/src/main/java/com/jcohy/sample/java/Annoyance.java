bf6ac719875cae9acb21fd2bc1bc10767fa63bab
Jcohy <jia_chao23@126.com>
2023-10-18 16:18:20 +0800

    add mysql guide

---
+++ java-template/src/main/java/com/jcohy/sample/java/Annoyance.java
@@ -0,0 +1,11 @@
+package com.jcohy.sample.java;
+
+/**
+ * Copyright: Copyright (c) 2023 <a href="https://www.jcohy.com" target="_blank">jcohy.com</a>
+ * <p> Description:
+ *
+ * @author jiac
+ * @version 2023.0.1 2023/10/12:10:20
+ * @since 2023.0.1
+ */
+class Annoyance extends Exception {}
